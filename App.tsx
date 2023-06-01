531b3201b1cae79b34e745c0cea909343eabc187
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-06-01 10:58:53 +0900

    initial commit!

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Overview'
+          }}
+          />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
